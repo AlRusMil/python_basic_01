b28a71432bfdb06fde5811616c34974788cc9150
AlRusMil <rusmil@yahoo.com>
2020-11-11 22:31:59 +0300

    les 2 test

--- homeworks/les2/task1.py
+++ homeworks/les2/task1.py
@@ -1,4 +1,4 @@
-some_list = [1, 5, "ffffgede", 0, True, [1, "r3r3r3"], "fefe", (1, ), (1, 5), {1:"as", "eee":"4343"}]
+some_list = [1, 5, "aaaffffgede", 0, True, [1, "r3r3r3"], "fefe", (1, ), (1, 5), {1:"as", "eee":"4343"}]
 
 for item_index in range(len(some_list)):
     print(f'{item_index}й элемент - {some_list[item_index]} является: {type(some_list[item_index])}')
